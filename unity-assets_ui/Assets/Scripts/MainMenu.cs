ea676afeef3158ea3fca384c419d822314f88009
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-03-06 13:50:49 -0600

    back button in options menue now returns to previous scene. Created Pause menue prefab

---
+++ unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Loads scenes
+    public void LevelSelect(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+        Debug.Log("Exited");
+    }
+}
