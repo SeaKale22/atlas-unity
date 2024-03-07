fdbbc521b7ae3f8ffb9fcd404fbe368db193d21b
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-03-07 11:25:42 -0600

    options and return

--- unity-assets_ui/Assets/Scripts/CameraController.cs
+++ unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -8,22 +8,26 @@ public class CameraController : MonoBehaviour
     public GameObject player;
     public Vector3 offset;
     public float rotationSpeed;
+    public bool moveCam = true;
 
     void Start()
     {
         // lock cursor in center of screen
-        Cursor.lockState = CursorLockMode.Locked;
+        // Cursor.lockState = CursorLockMode.Locked;
     }
     
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        if (moveCam)
+        {
+            transform.position = player.transform.position + offset;
 
-        Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
 
-        offset = camTurnAngle * offset;
-        transform.LookAt(player.transform);
+            offset = camTurnAngle * offset;
+            transform.LookAt(player.transform);
+        }
     }
 
 }
